ee9eca2745ccfb7214312235225bcf391f4f6965
Yunning <ayh95944674@gmail.com>
2026-03-04 22:43:10 +0900

    feat : 결제 관련 스키마 생성

---
+++ model/AiQuota.js
@@ -0,0 +1,38 @@
+// 유저의 AI 사용 가능 횟수와 리셋 규칙 관리
+
+//*** mongoose 세팅 ***//
+const mongoose = require("mongoose");
+
+const aiQuotaSchema  = new mongoose.Schema(
+    {
+        userId :{ type: mongoose.ObjectId, ref: User, required: true,},
+
+        // 무료 플랜 : Free 사용 가능 횟수
+        freeRemaing :{ type:Number, default : 3, min:0, max:3},
+        freeCycleAnchorAt :{ type:Date}, // 무료 플랜 시작일
+        freeResetAt : { type :Date}, // 무료 플랜 시작일 + 1개월(달력기준)
+
+        // 구독 플랜 : Basic, Premium
+        subExtraRemaining: { type: Number, default: 0, min: 0 }, // 비구독 사용자는 0 -> default:0
+        subExtraResetAt: { type: Date }, // 다음 결제일 = Subscription.currentPeriodEnd
+
+        // - 기간마다 지급되는 추가분
+        subGrantPerPeriod: { type: Number, default: 0, min: 0 }, // basic : 2추가, premium : 5 추가
+        subCarryCap: { type: Number, default: 0, min: 0 }, // 이월 최대치(2배수까지만)
+
+        // 1회성 충전 크래딧, 만료 기간 없음
+        topupRemaining: { type: Number, default: 0, min: 0 }, // 기본유저 충전 안 한 상태 -> default:0 
+
+        // 실패시 차감 0 관리
+        holds: { 
+            topup: { type: Number, default: 0, min: 0 },
+            subExtra: { type: Number, default: 0, min: 0 },
+            free: { type: Number, default: 0, min: 0 },
+        },
+    }
+    ,{ timestamps: true }
+);
+
+
+const AiQuota = mongoose.model("AiQuota", aiQuotaSchema );
+module.exports = AiQuota;
