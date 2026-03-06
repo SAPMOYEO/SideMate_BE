d978b174da4263ac9f3246f43dcc11569171224c
Yunning <ayh95944674@gmail.com>
2026-03-06 18:59:20 +0900

    feat : 결제, ai 사용량, ai 사용규칙 라우팅

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const paymentApi = require("./payment.api");
+const aiQuotaApi = require("./aiQuota.api");
+const aiUsageApi = require("./aiUsage.api");
 
 router.use("/payment", paymentApi);
+router.use("/aiquota", aiQuotaApi);
+router.use("/aiusage", aiUsageApi);
 
 module.exports = router;
