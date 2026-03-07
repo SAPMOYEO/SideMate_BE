cdbc460db1543a7c5c1674bf48c9223ee62f7efe
Hannah Jung <hj.hannahjung@gmail.com>
2026-03-06 19:59:14 -0800

    feat: 유저 프로필 수정 및 프로젝트 라우트 업데이트

--- routes/user.api.js
+++ routes/user.api.js
@@ -6,5 +6,6 @@ const authController = require("../controller/auth.controller");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/me", authController.authenticate, userController.getUser);
+router.put("/me", authController.authenticate, userController.updateUser);
 
 module.exports = router;
